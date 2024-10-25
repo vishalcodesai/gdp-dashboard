393de4b1c53cccf91a644561900ec9638208accb
vishalcodesai <vishalnait.94@gmail.com>
2024-10-25 23:43:48 +0000

    changing minimum year to 2000

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ def get_gdp_data():
     DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
     raw_gdp_df = pd.read_csv(DATA_FILENAME)
 
-    MIN_YEAR = 1960
+    MIN_YEAR = 2000
     MAX_YEAR = 2022
 
     # The data above has columns like:
